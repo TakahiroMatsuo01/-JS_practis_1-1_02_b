ae155ad3d5f1513e3ed38065dbe2975ba7566fd7
Takahiro matsuo <miurugi.discord@gmail.com>
2021-12-31 13:08:26 +0900

    再修正3

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 for(let i = 1; i < 100 ; i++){
 
     if( i % 3 === 0 && i % 5 === 0) {
-        console.log(`FizzBuzz　${i}`);
+        console.log(`FizzBuzz\b${i}`);
     }else if( i % 5 === 0) {
-        console.log(`Buzz ${i}`);
+        console.log(`Buzz\b${i}`);
     }else if ( i % 3 === 0) {
-        console.log(`Fizz ${i}`);
+        console.log(`Fizz\b${i}`);
     }
 }
